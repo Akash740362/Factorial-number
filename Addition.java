75a5d6627bc0a358cefc7a5d024daa93149064b4
Akash740362 <gauakash01@gmail.com>
2024-10-21 13:31:17 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Addition {
     public static void main(String args[]) {
-        System.out.println("Hello world");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the first number:");
+        int a = sc.nextInt();
+        Systrm.out.print("Please enter the second number:");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.print("Addition is:" + sum);
     }
 }
